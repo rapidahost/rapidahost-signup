a231ba062881414b90099b6bde014551fefa1b07
rapidahost <rapidahost@gmail.com>
2025-06-30 18:02:04 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>ระบบสมัครสมาชิก Rapidahost</h1>
+      <p>ไปที่ <a href="/signup">หน้าสมัครสมาชิก</a></p>
+    </div>
+  );
+}
