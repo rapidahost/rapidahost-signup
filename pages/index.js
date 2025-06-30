11028d6a1bf3b65e42b985e5cc21f6a43295210b
rapidahost <rapidahost@gmail.com>
2025-06-30 18:24:52 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Rapidahost Sign Up</h1>
+      <p>ระบบสมัครสมาชิกพร้อม Firebase Auth</p>
+    </div>
+  );
+}
