a03c120b96db03521dd6be53b026b464c92f324a
rapidahost <rapidahost@gmail.com>
2025-07-02 00:32:24 +0700

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,15 +1,16 @@
 // firebase/config.js
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyAd7JupXNq9ALK77JcVhmTBmSPLUii19R8",
+  authDomain: "rapidahost-a8660.firebaseapp.com",
+  projectId: "rapidahost-a8660",
+  storageBucket: "rapidahost-a8660.appspot.com",
+  messagingSenderId: "435134848810",
+  appId: "1:435134848810:web:0d1ff31cfa851bdbe14853"
 };
 
 const app = initializeApp(firebaseConfig);
 export default app;
+
 
