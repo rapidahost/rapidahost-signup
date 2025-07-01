5d757b811c1c97d810fa221c4f53f7990913aed8
rapidahost <rapidahost@gmail.com>
2025-07-01 17:33:59 +0700

    Update signup.jsx

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,4 +1,15 @@
+import React from 'react';
+
 export default function Signup() {
-  return <div>Sign Up</div>;
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form>
+        <input type="email" placeholder="Email" /><br />
+        <input type="password" placeholder="Password" /><br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
