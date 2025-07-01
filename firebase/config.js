e64698f332b3a52fa3f496e4bf4a2699e997cee1
rapidahost <rapidahost@gmail.com>
2025-07-01 18:43:20 +0700

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,18 +1,15 @@
-// /firebase/config.js
+// firebase/config.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0PHiq8KuosFDUu9oDbQkBULJfc5TWTBk",
-  authDomain: "rapidahost-a8660.firebaseapp.com",
-  projectId: "rapidahost-a8660",
-  storageBucket: "rapidahost-a8660.appspot.com",
-  messagingSenderId: "387960126750",
-  appId: "1:387960126750:web:27cd9e3c40c06f6c3298a1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase App
 const app = initializeApp(firebaseConfig);
-
-// Export Authentication Instance
 export const auth = getAuth(app);
