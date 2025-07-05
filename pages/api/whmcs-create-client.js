e6e4c27355c5df0375a4ee12a8b25e8a8cba6fd8
rapidahost <rapidahost@gmail.com>
2025-07-05 09:05:54 +0700

    Update whmcs-create-client.js

--- pages/api/whmcs-create-client.js
+++ pages/api/whmcs-create-client.js
@@ -1,11 +1,13 @@
 const response = await fetch("https://billing.rapidahost.com/api-create-client.php", {
   method: "POST",
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
   },
   body: JSON.stringify({
+    accesskey: process.env.WgxMFWsrvlNgYjy36h2taCX4nSBLKbnx,  // ✅ ใส่ accesskey ที่นี่!
     action: "CreateClient",
-    ...req.body
+    ...req.body,
   }),
 });
+
 
