5395bc1f45caa6a80e5969a0597044bd64c131a6
rapidahost <rapidahost@gmail.com>
2025-07-06 17:47:36 +0700

    Update whmcs-create-client.js

--- pages/api/whmcs-create-client.js
+++ pages/api/whmcs-create-client.js
@@ -1,34 +1,15 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
-  const {
+const response = await fetch("https://billing.rapidahost.com/api-create-client.php", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
     firstname,
     lastname,
     email,
-    phonenumber,
     password,
+    phonenumber,
     firebase_uid,
-  } = req.body;
+  }),
+});
 
-  try {
-    const whmcsResponse = await fetch("https://billing.rapidahost.com/api-create-client.php", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        firstname,
-        lastname,
-        email,
-        phonenumber,
-        password,
-        firebase_uid,
-      }),
-    });
-
-    const result = await whmcsResponse.json();
-    return res.status(200).json(result);
-  } catch (error) {
-    return res.status(500).json({ error: "WHMCS relay failed", detail: error.message });
-  }
-}
