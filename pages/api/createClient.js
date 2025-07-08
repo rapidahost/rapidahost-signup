fda56a5bbde1fb45de4c31571c2256d3aa940a1b
rapidahost <rapidahost@gmail.com>
2025-07-08 11:47:33 +0700

    Update createClient.js

--- pages/api/createClient.js
+++ pages/api/createClient.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
       firstname: 'New',
       lastname: 'Client',
       country: 'TH',
-      responseType: 'json',
+      responsetype: 'json',
     });
 
     const authHeader = 'WHMCS ' + Buffer.from(`${identifier}:${secret}`).toString('base64');
